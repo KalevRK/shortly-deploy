088d741d075909f5e058c52439f38e01b1c2338f
Hack Reactor Students <accounts@hackreactor.com>
2015-03-04 19:31:06 -0800

    Fixed password hashing issue & links not returning

--- app/config.js
+++ app/config.js
@@ -8,7 +8,6 @@ var Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/shortly');
 
 module.exports.linkSchema = new mongoose.Schema({
-  // id: Schema.ObjectId,
   url: String,
   base_url: String,
   code: String,
@@ -25,49 +24,17 @@ module.exports.linkSchema.pre('save', function(next){
 })
 
 module.exports.userSchema = new mongoose.Schema({
-  // id: Schema.ObjectId,
   username: String,
   password: String,
   timestamp: {type: Date, default: Date.now}
 });
 
 module.exports.userSchema.pre('save', function(next) {
-  this.comparePassword = 'TACOOOOOO';
-  (function(){
-      var cipher = Promise.promisify(bcrypt.hash);
-      return cipher(this.password, null, null).bind(this)
-        .then(function(hash) {
-          this.password = hash;
-        });
-  })();
+  var cipher = Promise.promisify(bcrypt.hash);
+  cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.update({password: hash}, function(err){
+      });
+    });
   next();
 });
-
-
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
